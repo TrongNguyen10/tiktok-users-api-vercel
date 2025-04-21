7115f9304f16f7ff0e851e34524097c985d6a7b0
TrongNguyen10 <trungtrung1220@gmail.com>
2025-04-21 13:12:58 +0700

    add cors options

--- api/server.js
+++ api/server.js
@@ -1,7 +1,11 @@
 // See https://github.com/typicode/json-server#module
 const jsonServer = require('json-server')
 const cors = require('cors')
-
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    credentials: true, //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+}
 const server = jsonServer.create()
 
 // Uncomment to allow write operations
@@ -17,7 +21,7 @@ const router = jsonServer.router('db.json')
 
 const middlewares = jsonServer.defaults()
 
-server.use(cors())
+server.use(cors(corsOptions))
 
 server.use(middlewares)
 // Add this before server.use(router)
@@ -28,7 +32,7 @@ server.use(
     }),
 )
 server.use(router)
-server.listen(3001, () => {
+server.listen(3000, () => {
     console.log('JSON Server is running')
 })
 
