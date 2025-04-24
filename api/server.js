2db124d91b4fbfff12c53bdcddf578ce01d3b727
TrongNguyen10 <trungtrung1220@gmail.com>
2025-04-24 23:33:33 +0700

    update server.js

--- api/server.js
+++ api/server.js
@@ -12,25 +12,6 @@ const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
 
-// ✅ CORS middleware cho cả local + production
-server.use((req, res, next) => {
-    const allowedOrigins = ['http://localhost:3000', 'https://ndt-tiktokui.netlify.app']
-    const origin = req.headers.origin
-
-    if (allowedOrigins.includes(origin)) {
-        res.setHeader('Access-Control-Allow-Origin', origin)
-    }
-
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
-
-    if (req.method === 'OPTIONS') {
-        return res.status(200).end()
-    }
-
-    next()
-})
-
 server.use(
     jsonServer.rewriter({
         '/api/*': '/$1',
